57a215fd9f5824894763d9fe3b36438985a5509c
Paulino Auyon <pauyon@jgboswell.com>
2023-05-22 10:05:44 -0700

    Add form validation

--- BlazorShopDemo2.Models/CategoryDto.cs
+++ BlazorShopDemo2.Models/CategoryDto.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazorShopDemo2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShopDemo2.Models
 {
     public class CategoryDto
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
     }
 }
