efee504a53c1166f5ab9ae3ede3c33141cdba80d
Paulino Auyon <pauyon@jgboswell.com>
2023-05-23 16:04:21 -0700

    Update to .net 7 and added identity

--- BlazorShop2.Api/Program.cs
+++ BlazorShop2.Api/Program.cs
@@ -2,6 +2,8 @@ using BlazorShopDemo2.Business.Repository.IRepository;
 using BlazorShopDemo2.Business.Repository;
 using BlazorShopDemo2.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using BlazorShopDemo2.Domain.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
--- BlazorShopDemo2.DataAccess/Data/ApplicationDbContext.cs
+++ BlazorShopDemo2.DataAccess/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using BlazorShopDemo2.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BlazorShopDemo2.DataAccess.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
